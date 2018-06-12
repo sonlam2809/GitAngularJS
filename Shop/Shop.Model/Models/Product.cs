c978244659de4c34d499611769c0389929c01e1e
lamns6 <lamns6@300-FRS-V423>
2018-06-12 17:00:59 +0700

    Add model

---
+++ Shop/Shop.Model/Models/Product.cs
@@ -0,0 +1,14 @@
+﻿using Shop.Model.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shop.Model.Models
+{
+    public class Product: Auditable
+    {
+
+    }
+}
